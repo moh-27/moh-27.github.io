4901a17988672f87a181f6fb0520eb07f35f8161
Mohammed Alsaggaf <30930306+moh-27@users.noreply.github.com>
2021-08-22 23:41:46 +0300

    Mail sender php

--- mail.php
+++ mail.php
@@ -1,50 +1,52 @@
 <?php
-$name = trim($_POST['contact-name']);
-$phone = trim($_POST['contact-phone']);
-$email = trim($_POST['contact-email']);
-$message = trim($_POST['contact-message']);
-if ($name == "") {
-    $msg['err'] = "\n Name can not be empty!";
-    $msg['field'] = "contact-name";
-    $msg['code'] = FALSE;
-} else if ($phone == "") {
-    $msg['err'] = "\n Phone number can not be empty!";
-    $msg['field'] = "contact-phone";
-    $msg['code'] = FALSE;
-} else if (!preg_match("/^[0-9 \\-\\+]{4,17}$/i", trim($phone))) {
-    $msg['err'] = "\n Please put a valid phone number!";
-    $msg['field'] = "contact-phone";
-    $msg['code'] = FALSE;
-} else if ($email == "") {
-    $msg['err'] = "\n Email can not be empty!";
-    $msg['field'] = "contact-email";
-    $msg['code'] = FALSE;
-} else if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-    $msg['err'] = "\n Please put a valid email address!";
-    $msg['field'] = "contact-email";
-    $msg['code'] = FALSE;
-} else if ($message == "") {
-    $msg['err'] = "\n Message can not be empty!";
-    $msg['field'] = "contact-message";
-    $msg['code'] = FALSE;
-} else {
-    $to = 'moh.27@hotmail.com';
-    $subject = 'Papr Contact Query';
-    $_message = '<html><head></head><body>';
-    $_message .= '<p>Name: ' . $name . '</p>';
-    $_message .= '<p>Message: ' . $phone . '</p>';
-    $_message .= '<p>Email: ' . $email . '</p>';
-    $_message .= '<p>Message: ' . $message . '</p>';
-    $_message .= '</body></html>';
+if (isset($_POST['submit'])) {
+    $name = trim($_POST['contact-name']);
+    $phone = trim($_POST['contact-phone']);
+    $email = trim($_POST['contact-email']);
+    $message = trim($_POST['contact-message']);
+    if ($name == "") {
+        $msg['err'] = "\n Name can not be empty!";
+        $msg['field'] = "contact-name";
+        $msg['code'] = FALSE;
+    } else if ($phone == "") {
+        $msg['err'] = "\n Phone number can not be empty!";
+        $msg['field'] = "contact-phone";
+        $msg['code'] = FALSE;
+    } else if (!preg_match("/^[0-9 \\-\\+]{4,17}$/i", trim($phone))) {
+        $msg['err'] = "\n Please put a valid phone number!";
+        $msg['field'] = "contact-phone";
+        $msg['code'] = FALSE;
+    } else if ($email == "") {
+        $msg['err'] = "\n Email can not be empty!";
+        $msg['field'] = "contact-email";
+        $msg['code'] = FALSE;
+    } else if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+        $msg['err'] = "\n Please put a valid email address!";
+        $msg['field'] = "contact-email";
+        $msg['code'] = FALSE;
+    } else if ($message == "") {
+        $msg['err'] = "\n Message can not be empty!";
+        $msg['field'] = "contact-message";
+        $msg['code'] = FALSE;
+    } else {
+        $to = 'moh.27@hotmail.com';
+        $subject = 'Papr Contact Query';
+        $_message = '<html><head></head><body>';
+        $_message .= '<p>Name: ' . $name . '</p>';
+        $_message .= '<p>Message: ' . $phone . '</p>';
+        $_message .= '<p>Email: ' . $email . '</p>';
+        $_message .= '<p>Message: ' . $message . '</p>';
+        $_message .= '</body></html>';
 
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From:  Papr <moh.27@hotmail.com>' . "\r\n";
-    $headers .= 'cc: moh.27@hotmail.com' . "\r\n";
-    $headers .= 'bcc: moh.27@hotmail.com' . "\r\n";
-    mail($to, $subject, $_message, $headers, '-f moh.27@hotmail.com');
-
-    $msg['success'] = "\n Email has been sent successfully.";
-    $msg['code'] = TRUE;
+        $headers = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $headers .= 'From:  Papr <moh.27@hotmail.com>' . "\r\n";
+        $headers .= 'cc: moh.27@hotmail.com' . "\r\n";
+        $headers .= 'bcc: moh.27@hotmail.com' . "\r\n";
+        if(mail($to, $subject, $_message, $headers, '-f moh.27@hotmail.com')){
+        $msg['success'] = "\n Email has been sent successfully.";
+        $msg['code'] = TRUE;
+        }
+    }
 }
 echo json_encode($msg);
